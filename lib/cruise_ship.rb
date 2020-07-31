2607931c9f2fc7c12246550360ef5b58dbc81b04
Daniel Gould <dmgould310@gmail.com>
2020-07-31 21:47:08 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,14 +16,3 @@ passengers.each do |suite, name|
   winner 
 end 
 
-birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
